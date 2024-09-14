2dddd7835f61585e1457f549469d0843d8b3ca64
Cédric Hennequin <niquenen@users.noreply.github.com>
2024-09-14 15:48:23 +0200

    build: add commitlint configuration

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+/**
+ * @author Cédric Hennequin
+ * @company H2V Solutions
+ * @created_at 2024-09-13 10:23:17
+ * @updated_by Cédric Hennequin
+ * @updated_at 2024-09-13 11:00:08
+ */
+
+// @ts-check
+
+export default {
+	extends: [
+		'@commitlint/config-conventional'
+	]
+};
